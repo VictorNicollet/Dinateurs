96ecf9038d2c5f27db283484764964f769bfccf9
Victor Nicollet <victor.nicollet@lokad.com>
2026-05-29 18:12:21 +0200

    Turn it into a flake

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "NixOS configuration";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      hostName = "hostname";
+    in
+    {
+      nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          ({ pkgs, ... }: {
+            networking.hostName = hostName;
+            networking.networkmanager.enable = true;
+
+            services.xserver.enable = true;
+            services.xserver.displayManager.gdm.enable = true;
+            services.xserver.desktopManager.gnome.enable = true;
+
+            services.gnome.gnome-keyring.enable = true;
+            security.pam.services.login.enableGnomeKeyring = true;
+
+            environment.systemPackages = with pkgs; [
+              bind.dnsutils
+              git
+              iproute2
+              nettools
+              seahorse
+            ];
+
+            services.unbound = {
+              enable = true;
+              settings.server = {
+                interface = [
+                  "127.0.0.1"
+                  "::1"
+                ];
+                access-control = [
+                  "127.0.0.0/8 allow"
+                  "::1 allow"
+                ];
+              };
+            };
+
+            networking.nameservers = [
+              "127.0.0.1"
+              "::1"
+            ];
+
+            services.openssh = {
+              enable = true;
+              settings = {
+                PasswordAuthentication = false;
+                PermitRootLogin = "prohibit-password";
+              };
+            };
+
+            users.users.root.openssh.authorizedKeys.keyFiles = [
+              ./authorized_keys
+            ];
+
+            system.stateVersion = "25.11";
+          })
+        ];
+      };
+    };
+}
