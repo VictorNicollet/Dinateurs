992ed8b1fd67e5bfe80d683f54536cf6f7bb9463
Victor Nicollet <victor.nicollet@lokad.com>
2026-05-29 18:04:50 +0200

    Root nix file, for starters

---
+++ root.nix
@@ -0,0 +1,51 @@
+{ pkgs, ... }:
+
+{
+  networking.networkmanager.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.login.enableGnomeKeyring = true;
+
+  environment.systemPackages = with pkgs; [
+    bind.dnsutils
+    git
+    iproute2
+    nettools
+    seahorse
+  ];
+
+  services.unbound = {
+    enable = true;
+    settings.server = {
+      interface = [
+        "127.0.0.1"
+        "::1"
+      ];
+      access-control = [
+        "127.0.0.0/8 allow"
+        "::1 allow"
+      ];
+    };
+  };
+
+  networking.nameservers = [
+    "127.0.0.1"
+    "::1"
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "prohibit-password";
+    };
+  };
+
+  users.users.root.openssh.authorizedKeys.keyFiles = [
+    ./authorized_keys
+  ];
+}
